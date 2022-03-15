1cc3fbb8bdb22053fa9759e48065ef3827088d15
Arnold <arami265@gmail.com>
2022-03-15 14:53:20 -0500

    updated project title to match repo

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/pw'),
+      dir: require('path').join(__dirname, './coverage/angular-password-generator'),
       subdir: '.',
       reporters: [
         { type: 'html' },
