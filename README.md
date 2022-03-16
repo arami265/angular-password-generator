7859af80edc06b453a029228bb3b4c51d9cb24b2
Arnold <arami265@gmail.com>
2022-03-16 16:06:01 -0500

    Added feature, small improvements
    Added feature supporting unallowed characters; added README contents; changed styling slightly; improved Github link. Also made slight formatting changes.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # angular-password-generator
  
+This project is my implementation of a fairly common Angular app, inspired by one of the projects in the Modern Angular Bootcamp course.
+
+It has the following added features:
+
+- Hand stylized via Angular Material components and theming, and some custom CSS
+- Takes unallowed characters into account via regular expression
+- Improved logic for efficiency and compatibility with latest Angular version, including improved type checking, and passing of event targets rather than entire events
+
+### [Visit the app here on Github Pages!](https://arami265.github.io/angular-password-generator/) (It can lead you back here)
+<br>
+
+## TODO:
+- Add additional pages for additional generation methods
+- Tweak styling
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   length = 0;
+  unallowed = '';
   useLetters = false;
   useNumbers = false;
   useSymbols = false;
@@ -19,9 +20,17 @@ export class AppComponent {
     if(Number.isInteger(parsedValue)) {
       this.length = parsedValue;
     }
-    else {
-      this.length = 0;
+    else this.length = 0;
+  }
+
+  onInputUnallowed(target: EventTarget) {
+    const value = (target as HTMLInputElement).value;
+    const unallowed = value;
+
+    if(unallowed.length > 0) {
+      this.unallowed = unallowed;
     }
+    else this.unallowed = '';
   }
 
   onChangeUseLetters() {
@@ -48,6 +57,7 @@ export class AppComponent {
     // console.log('letters: ' + this.useLetters);
     // console.log('numbers: ' + this.useNumbers);
     // console.log('symbols: ' + this.useSymbols);
+    // console.log(this.unallowed);
 
     const letters = 'qwertyuiopasdfghjklzxcvbnm';
     const numbers = '1234567890';
@@ -65,11 +75,17 @@ export class AppComponent {
       validChars += symbols;
     }
 
+    if(this.unallowed.length > 0) {
+      for (let i = 0; i < this.unallowed.length; i++) {
+        validChars = validChars.replace(this.unallowed[i], '');
+      }
+    }
+
     let generatedPassword = '';
     for (let i = 0; i < this.length; i++) {
-      const index = Math.floor(Math.random() * validChars.length);
+      const randIndex = Math.floor(Math.random() * validChars.length);
 
-      generatedPassword += validChars[index];
+      generatedPassword += validChars[randIndex];
     }
 
     this.password = generatedPassword;
